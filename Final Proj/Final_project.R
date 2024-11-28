90dded64b9c10fe6cd7856e0b409ebaaeef0461d
xiaotin22 <terry941020@gmail.com>
2024-11-28 14:03:55 +0800

    ttt

--- Final Proj/Final_project.R
+++ Final Proj/Final_project.R
@@ -7,8 +7,6 @@ library(ape)
 library(dplyr)
 library(ggfortify)
 
-rm(filtered_data)
-
 etAll <- data
 
 etAll$score2016 <- 20 * etAll$gold2016 + 7 * etAll$silver2016 + 5 * etAll$bronze2016
@@ -16,15 +14,14 @@ etAll$score2020 <- 20 * etAll$gold2020 + 7 * etAll$silver2020 + 5 * etAll$bronze
 etAll$score2024 <- 20 * etAll$gold2024 + 7 * etAll$silver2024 + 5 * etAll$bronze2024
 
 plot(etAll$score2024
-     ,log(etAll$GDP2024) ,
-     main = "2024",
-     xlab = "score_percent",
-     ylab = "GDP",
+     , log (etAll$GDP2024) ,
+     main = "2024 Paris",
+     xlab = "score (20,7,5)",
+     ylab = "log(GDP)",
      col = "blue",
      pch = 19)
 
-model
-model <- lm(score2024 ~ log(GDP2024)+log(pop2024), data=etAll)
+model <- lm(score2024 ~ log(GDP2024) + log(population2024), data=etAll)
 summary(model)
 
 
