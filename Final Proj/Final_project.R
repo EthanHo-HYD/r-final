696545fb3da588f310b0a1dc20c2df59148bdd99
xiaotin22 <terry941020@gmail.com>
2024-11-28 14:55:41 +0800

    ttt

--- Final Proj/Final_project.R
+++ Final Proj/Final_project.R
@@ -13,19 +13,20 @@ etAll$score2016 <- 20 * etAll$gold2016 + 7 * etAll$silver2016 + 5 * etAll$bronze
 etAll$score2020 <- 20 * etAll$gold2020 + 7 * etAll$silver2020 + 5 * etAll$bronze2020
 etAll$score2024 <- 20 * etAll$gold2024 + 7 * etAll$silver2024 + 5 * etAll$bronze2024
 
-plot(etAll$score2024
-     , log (etAll$GDP2024) ,
-     main = "2024 Paris",
-     xlab = "score (20,7,5)",
-     ylab = "log(GDP)",
-     col = "blue",
-     pch = 19)
+ggplot(data = etAll, aes(x = score2024, y = log(GDP2024))) +
+  geom_point(color = "blue", shape = 20, size = 3) +  
+  ggtitle("2024 Paris") +
+  xlab("score: (20,7,5)") +
+  ylab("log (GDP)") +
+  theme(
+    plot.title = element_text(size = 20, face = "bold"),  
+    axis.title = element_text(size = 16),  
+    axis.text = element_text(size = 14)    
+  )
 
 model <- lm(score2024 ~ log(GDP2024) + log(population2024), data=etAll)
 summary(model)
 
-
-install.packages("iris")
 
 autoplot(model) #畫出模型診斷的圖
 shapiro.test(model$residual) #殘差項常態性檢定
