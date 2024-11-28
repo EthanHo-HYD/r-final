db13b860c0340e8d79f1e6f1e451929b94aa525b
xiaotin22 <terry941020@gmail.com>
2024-11-28 13:51:11 +0800

    ttt

--- Final Proj/Final_project.R
+++ Final Proj/Final_project.R
@@ -7,7 +7,13 @@ library(ape)
 library(dplyr)
 library(ggfortify)
 
+rm(filtered_data)
+
 etAll <- data
+
+etAll$score2016 <- 20 * etAll$gold2016 + 7 * etAll$silver2016 + 5 * etAll$bronze2016
+etAll$score2020 <- 20 * etAll$gold2020 + 7 * etAll$silver2020 + 5 * etAll$bronze2020
+etAll$score2024 <- 20 * etAll$gold2024 + 7 * etAll$silver2024 + 5 * etAll$bronze2024
 
 plot(etAll$score2024
      ,log(etAll$GDP2024) ,
@@ -22,7 +28,7 @@ model <- lm(score2024 ~ log(GDP2024)+log(pop2024), data=etAll)
 summary(model)
 
 
-#install.packages("ape")
+install.packages("iris")
 
 autoplot(model) #畫出模型診斷的圖
 shapiro.test(model$residual) #殘差項常態性檢定
