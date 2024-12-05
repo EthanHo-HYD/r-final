11a6c9f01ab366351a3f42b849be5edbab958d9f
Arthur-dei <ahr5998.ee13@nycu.edu.tw>
2024-12-05 16:54:49 +0800

    Create 三年間的複合成長率.R

---
+++ Final Proj/三年間的複合成長率.R
@@ -0,0 +1,7 @@
+years <- 4
+cagr <- ((GDP2024 / GDP2020)^(1 / years) - 1) * 100
+cat("從2020到2024複合成長率為:", round(cagr, 2), "%\n")
+
+years <- 4
+cagr <- ((GDP2020 / GDP2016)^(1 / years) - 1) * 100
+cat("從2016到2020複合成長率為:", round(cagr, 2), "%\n")
