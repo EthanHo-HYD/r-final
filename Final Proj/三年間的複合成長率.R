eac37c640e0e5e0bd86c2937f799a003e1b0b0e8
Arthur-dei <ahr5998.ee13@nycu.edu.tw>
2024-12-05 17:10:16 +0800

    Update 三年間的GDP、奧運積分複合成長率.R

--- Final Proj/三年間的複合成長率.R
+++ Final Proj/三年間的複合成長率.R
@@ -1,15 +1,15 @@
 years <- 4
 cagr <- ((GDP2024 / GDP2020)^(1 / years) - 1) * 100
-cat("從2020到2024複合成長率為:", round(cagr, 2), "%\n")
+cat("從2020到2024的各國GDP複合成長率為:", round(cagr, 2), "%\n")
 
 years <- 4
 cagr <- ((GDP2020 / GDP2016)^(1 / years) - 1) * 100
-cat("從2016到2020複合成長率為:", round(cagr, 2), "%\n")
+cat("從2016到2020的各國GDP複合成長率為:", round(cagr, 2), "%\n")
 
 years <- 4
-cagr <- ((GDP2024 / GDP2020)^(1 / years) - 1) * 100
-cat("從2020到2024複合成長率為:", round(cagr, 2), "%\n")
+cagr <- ((total2024score / total2020score)^(1 / years) - 1) * 100
+cat("從2020到2024的各國奧運積分複合成長率為:", round(cagr, 2), "%\n")
 
 years <- 4
 cagr <- ((GDP2020 / GDP2016)^(1 / years) - 1) * 100
-cat("從2016到2020複合成長率為:", round(cagr, 2), "%\n")
+cat("從2016到2020的各國奧運積分複合成長率為:", round(cagr, 2), "%\n")
